4e8c1c7a66bb0175b8a07ea1948d8068be704ddb
Мальцев Николай <Malzewn@gmail.com>
2024-01-23 17:37:16 +0300

    комменты добавил

--- proj.py
+++ proj.py
@@ -1,4 +1,4 @@
-# Используется мешок слов + логическая регрессия
+# Импорт необходимых библиотек и модулей
 import pandas as pd
 import numpy as np
 import pymorphy3
@@ -9,15 +9,20 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from collections import Counter
 
+
 # Загрузка данных из nltk для токенизации и список стоп-слов
 nltk.download('punkt')
 nltk.download('stopwords')
 
+# Задание максимального количества слов и случайного состояния для воспроизводимости результатов
 max_words = 10000
 random_state = 42
 
+
+# Загрузка данных из файла 'banks.csv' и установка индекса
 banks = pd.read_csv('banks.csv', sep='\t', index_col='idx')
 
+# Определение функции предобработки текста
 def preprocess(text, stop_words, punctuation_marks, morph):
     tokens = word_tokenize(text.lower())
     preprocessed_text = []
@@ -28,49 +33,62 @@ def preprocess(text, stop_words, punctuation_marks, morph):
                 preprocessed_text.append(lemma)
     return preprocessed_text
 
+
+# Задание списка пунктуационных знаков и загрузка стоп-слов для русского языка
 punctuation_marks = ['!', ',', '(', ')', ':', '-', '?', '.', '..', '...', '«', '»', ';', '–', '--']
 stop_words = stopwords.words("russian")
 morph = pymorphy3.MorphAnalyzer()
 
+
+# Применение функции предобработки ко всем текстам в датафрейме
 banks['Preprocessed_texts'] = banks.apply(lambda row: preprocess(row['Text'], punctuation_marks, stop_words, morph), axis=1)
 
+# Использование Counter для подсчета частоты слов
 words = Counter()
 
 for txt in banks['Preprocessed_texts']:
     words.update(txt)
 
-# Словарь, отображающий слова в коды
+
+# Создание словарей для отображения слов в индексы и наоборот
 word_to_index = dict()
-# Словарь, отображающий коды в слова
 index_to_word = dict()
 
 for i, word in enumerate(words.most_common(max_words - 2)):
     word_to_index[word[0]] = i + 2
     index_to_word[i + 2] = word[0]
 
+
+# Функция для преобразования текста в последовательность индексов
 def text_to_sequence(txt, word_to_index):
     seq = []
     for word in txt:
-        index = word_to_index.get(word, 1) # 1 означает неизвестное слово
-        # Неизвестные слова не добавляем в выходную последовательность
+        index = word_to_index.get(word, 1)  # 1 означает неизвестное слово
         if index != 1:
             seq.append(index)
     return seq
 
+
+# Применение функции text_to_sequence ко всем текстам в датафрейме
 banks['Sequences'] = banks.apply(lambda row: text_to_sequence(row['Preprocessed_texts'], word_to_index), axis=1)
 
+# Замена меток классов 'Negative' и 'Positive' числовыми значениями 0 и 1
 mapping = {'Negative': 0, 'Positive': 1}
-
 banks.replace({'Score': mapping}, inplace=True)
 
+
+# Разделение данных на обучающий и тестовый наборы
 train, test = train_test_split(banks, test_size=0.2)
 
+# Получение входных данных и меток классов для обучающего и тестового наборов
 x_train_seq = train['Sequences']
 y_train = train['Score']
 
 x_test_seq = test['Sequences']
 y_test = test['Score']
 
+
+# Функция для векторизации последовательностей
 def vectorize_sequences(sequences, dimension=10000):
     results = np.zeros((len(sequences), dimension))
     for i, sequence in enumerate(sequences):
@@ -78,18 +96,19 @@ def vectorize_sequences(sequences, dimension=10000):
             results[i, index] += 1.
     return results
 
+
+# Векторизация обучающего и тестового наборов
 x_train = vectorize_sequences(x_train_seq, max_words)
 x_test = vectorize_sequences(x_test_seq, max_words)
 
-x_train[0][:100]
-
-len(x_train[0])
-
+# Инициализация и обучение модели логистической регрессии
 lr = LogisticRegression(random_state=random_state, max_iter=500)
-
 lr.fit(x_train, y_train)
 
-lr.score(x_test, y_test)
+
+# Оценка точности модели на тестовом наборе
+print(lr.score(x_test, y_test))
+
 
 positive_text = """Банк мне не понравислся. Отношение сотрудников хуже некуда. Складывалось впечатление, что ты не нужен. На все вопросы отвечали агрессивно, времени пришлось потратить очень много. Не рекомендую!
 """
@@ -100,4 +119,10 @@ positive_bow = vectorize_sequences([positive_seq], max_words)
 positive_bow[0][0:100]
 result = lr.predict(positive_bow)
 
-print(result)
+
+if result == 0:
+    print("Негативный")
+elif result == 1:
+    print("Позитивный")
+else:
+    print("Оштбка")
